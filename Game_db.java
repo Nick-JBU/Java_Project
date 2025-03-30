9111e404ea9d706ced67e111dede46831ef535cf
gabrielpelkey12 <71278263+gp038341@users.noreply.github.com>
2025-03-30 15:57:33 -0500

    Add files via upload

---
+++ Game_db.java
@@ -0,0 +1,13 @@
+package mypack;
+import java.io.*;
+import java.sql.*;
+
+
+public class Game_db {
+	public static void main(String[] args) throws Exception{
+		String url = "jdbc:mysql://localhost:3306/gameDB";
+		String username = "root";
+		String password = "";
+		String query = "select*from emp"; // query to be run
+	}
+}
