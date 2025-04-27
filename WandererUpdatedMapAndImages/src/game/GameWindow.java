f41ba75fd4a0f320da63b143ba43360dffca2ecf
Nick-JBU <nicknlaw979@gmail.com>
2025-04-27 18:59:49 -0500

    Add files via upload

---
+++ WandererUpdatedMapAndImages/src/game/GameWindow.java
@@ -0,0 +1,31 @@
+package game;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class GameWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Wanderer");
+
+        try {
+            BufferedImage icon = ImageIO.read(GameWindow.class.getResourceAsStream("resources/Logo2.png"));
+            frame.setIconImage(icon);
+        } catch (IOException | NullPointerException e) {
+            System.err.println("Icon failed to load.");
+        }
+
+        GamePanel panel = new GamePanel();
+        panel.parentFrame = frame;
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel);
+        frame.setUndecorated(false);
+        frame.setSize(1600, 900);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        panel.requestFocusInWindow();
+    }
+}
